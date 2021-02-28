f11d83eb528db97388baa0c6fe2425c09df6859d
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-02-28 20:05:24 +0200

    implement a logging interceptor using a filter

---
+++ src/main/java/ocm/tinyiko/servlet/ParameterLoggingFilter.java
@@ -0,0 +1,37 @@
+package ocm.tinyiko.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class ParameterLoggingFilter implements Filter {
+
+    public ParameterLoggingFilter() {
+    }
+
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("A logging interceptor created with a filter::::");
+		request.getParameterMap().entrySet().stream().forEach(entry ->{
+			System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()[0]));
+		});
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
